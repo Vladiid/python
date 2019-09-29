4e712139ea7d220ecb2bf41c91b3865f077f8d2d
Vladiid <vladiid84@gmail.com>
2019-09-29 16:22:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-word = "Result: "
-number = 7
-num_2 = 5.6
+a = int(input("Enter num 1: "))
+b = int(input("Enter num 2: "))
 
-print(word)
+sum = a + b
+print("Sum = ", sum)
+minus = a - b
+print("Minus = ", minus)
+mult = a * b
+print("Mult = ", mult)
+delen = a / b
+print("Delen = ", delen)
+
